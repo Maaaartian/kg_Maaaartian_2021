a25d64d0c9e8278904e221e7942f1bfe877685b7
Mo Shi <mo.shi@rutgers.edu>
2020-03-18 20:40:27 -0400

    Add files via upload
    Added notes in the Python codes

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
 
 def solution(s, t):
-    if not len(s) == len(t): return False
-    map = {}
+    if not len(s) == len(t): return False # compare length of 2 strings
+    map = {} # hash table to store intermediate mapping results
     for i in range(len(s)):
+        # if a letter has appeared already and has been mapped to a new letter, return False
         if map.get(s[i]) and not map.get(s[i]) == t[i]: return False
-        map[s[i]] = t[i]
+        map[s[i]] = t[i] # update the hash table
     return True
 
 if __name__ == '__main__':
